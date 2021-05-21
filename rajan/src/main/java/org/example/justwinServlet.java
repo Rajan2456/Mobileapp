4cfc901c4f33f33a81c8c7f02ef20b47f8737df7
Rajan2456 <81096656+Rajan2456@users.noreply.github.com>
2021-05-20 22:59:43 -0400

    Update justwinServlet.java

--- rajan/src/main/java/org/example/justwinServlet.java
+++ rajan/src/main/java/org/example/justwinServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class justwinServlet extends HttpServlet {
+public class rajanServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws ServletException, IOException
